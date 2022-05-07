3df8ba868693a65d2f6bdacecc63d6b3b3340de6
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-05-07 19:32:59 +0200

    me funciona aparentemente la funcion para cambios

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pandas as pd
 
 
-def cambios_avg_area_income(lista):
+def cambios_letras(lista):
         lista_cambiada=[]
         for elementos in lista:
             if(elementos==''):
-                lista_cambiada.append('0')
+                pass
             else:
                 lista_cambiada.append(elementos)
+        return lista_cambiada
+
+def cambios_numeros(lista):
+        lista_cambiada=[]
+        for elementos in lista:
+            if(elementos==''):
+                pass
+            elif(isinstance(float(elementos),float)!=True):
+                pass
+            else:
+                lista_cambiada.append(float(elementos))
         return lista_cambiada
 
 
@@ -16,4 +27,10 @@ def main():
     # ----- LEER EL ARCHIVO CSV -----
     df=pd.read_csv('USA_Housing.csv')
 
-    # ----- 
+    # ----- CREANDO LAS LISTAS DE LOS DATOS -----
+    n_media_ganancias_area=df['Avg. Area Income'].tolist()
+    cambios_numeros(n_media_ganancias_area)
+    print(n_media_ganancias_area)
+
+if __name__=='__main__':
+    main()
