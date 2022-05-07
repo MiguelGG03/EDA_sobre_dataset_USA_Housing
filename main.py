b458eeac975c00d5f599d1b88827aea3e3557aec
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-05-07 19:56:24 +0200

    main con todas las listas

--- main.py
+++ main.py
@@ -29,8 +29,30 @@ def main():
 
     # ----- CREANDO LAS LISTAS DE LOS DATOS -----
     n_media_ganancias_area=df['Avg. Area Income'].tolist()
-    cambios_numeros(n_media_ganancias_area)
-    print(n_media_ganancias_area)
+    m_ganancias_area=cambios_numeros(n_media_ganancias_area)
+
+    n_media_edad_casas=df['Avg. Area House Age'].tolist()
+    m_edad_casas=cambios_numeros(n_media_edad_casas)
+
+    n_media_tamaño_habitacion=df['Avg. Area Number of Rooms'].tolist()
+    m_tamaño_h=cambios_numeros(n_media_tamaño_habitacion)
+
+    n_media_numero_habitaciones=df['Avg. Area Number of Rooms'].tolist()
+    m_n_habit=cambios_numeros(n_media_numero_habitaciones)
+
+    n_media_numero_bedrooms=df['Avg. Area Number of Bedrooms'].tolist()
+    m_n_bedrooms=cambios_numeros(n_media_numero_bedrooms)
+
+    n_media_poblacion=df['Area Population'].tolist()
+    m_poblacion=cambios_numeros(n_media_poblacion)
+
+    n_precio=df['Price'].tolist()
+    precio=cambios_numeros(n_precio)
+
+    n_direccion=df['Address'].tolist()
+    direccion=cambios_letras(n_direccion)
+
+
 
 if __name__=='__main__':
     main()
